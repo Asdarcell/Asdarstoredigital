a0a0d2bf9d1de8e236dc3e86f0b85d343ea179b8
Asdarcell <asdar123014@gmail.com>
2025-07-06 18:41:41 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_L6U3HKM1DnKuq_Q1zKFH4bk8GMAz210",
+  authDomain: "asdarstoredigitalll-d89c4.firebaseapp.com",
+  databaseURL: "https://asdarstoredigitalll-d89c4-default-rtdb.firebaseio.com",
+  projectId: "asdarstoredigitalll-d89c4",
+  storageBucket: "asdarstoredigitalll-d89c4.firebasestorage.app",
+  messagingSenderId: "220670500351",
+  appId: "1:220670500351:web:5737ae5958a6f5a67d5bca"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+document.getElementById('formPesanan').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const nama = document.getElementById('nama').value;
+  const nomor = document.getElementById('nomor').value;
+  const produk = document.getElementById('produk').value;
+  const waktu = new Date().toISOString();
+  const id = Date.now();
+
+  db.ref('pesanan/' + id).set({
+    id, nama, nomor, produk, status: "Menunggu Pembayaran", waktu
+  });
+
+  alert('Pesanan dikirim! Silakan transfer dan upload bukti pembayaran.');
+  location.href = "status.html?id=" + id;
+});
