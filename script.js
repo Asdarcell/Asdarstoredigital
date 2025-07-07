05bdda08c28ff2a4fc0802756914ef5323a8e3f6
Asdarcell <asdar123014@gmail.com>
2025-07-07 10:32:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA_L6U3HKM1DnKuq_Q1zKFH4bk8GMAz210",
+  apiKey: "f71ccc1a662c9f8656766990018d3a33",
   authDomain: "asdarstoredigitalll-d89c4.firebaseapp.com",
   databaseURL: "https://asdarstoredigitalll-d89c4-default-rtdb.firebaseio.com",
   projectId: "asdarstoredigitalll-d89c4",
@@ -9,10 +8,12 @@ const firebaseConfig = {
   appId: "1:220670500351:web:5737ae5958a6f5a67d5bca"
 };
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.database();
 
 document.getElementById('formPesanan').addEventListener('submit', function(e) {
   e.preventDefault();
+
   const nama = document.getElementById('nama').value;
   const nomor = document.getElementById('nomor').value;
   const produk = document.getElementById('produk').value;
@@ -21,15 +22,17 @@ document.getElementById('formPesanan').addEventListener('submit', function(e) {
 
   db.ref('pesanan/' + id).set({
     id, nama, nomor, produk, status: "Menunggu Pembayaran", waktu
+  }).then(() => {
+    alert(
+      '🟣 PESANAN DITERIMA 🟣\\n' +
+      'Silakan transfer pembayaran ke salah satu metode berikut:\\n\\n' +
+      'ShopeePay: 081803004607\\n' +
+      'DANA: 087755432880\\n\\n' +
+      'Setelah transfer, upload bukti pembayaran di halaman selanjutnya.'
+    );
+    location.href = "upload.html?id=" + id;
+  }).catch(err => {
+    alert('Gagal mengirim pesanan. Coba lagi.');
+    console.error(err);
   });
-
-  alert(
-    '🟣 PESANAN DITERIMA 🟣\n' +
-    'Silakan transfer pembayaran ke salah satu metode berikut:\n\n' +
-    'ShopeePay: 081803004607\n' +
-    'DANA: 087755432880\n\n' +
-    'Setelah transfer, upload bukti pembayaran di halaman selanjutnya.'
-  );
-
-  location.href = "upload.html?id=" + id;
 });
